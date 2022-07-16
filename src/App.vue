8035bf1bcb184232325c78419d167974b273b501
zaaraa-z <zamanian.zahra@gmail.com>
2022-07-16 17:33:19 +0200

    add styles

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,10 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -8,7 +8,7 @@
       <li v-for="(item, index) in books" :key="index">
         <p>
           <strong>{{ item.title }}</strong> by {{ item.author }} -
-          {{ item.pages }} pages
+          {{ item.pages }} pages <span>x</span>
         </p>
       </li>
     </ul>
@@ -39,20 +39,39 @@ export default {
 
 <style scoped>
 section {
+  width: 80%;
+  max-width: 700px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: auto;
+}
+
+form {
+  width: 100%;
+}
+
+form input {
+  width: 100%;
+  padding: 10px;
 }
 
 ul {
+  width: 100%;
   list-style: none;
   text-align: left;
-  padding-left: 0;
+  padding: 0;
 }
 li {
   background-color: rgba(179, 179, 241, 0.529);
   padding: 6px 20px;
   margin-bottom: 10px;
+}
+
+span {
+  color: rgb(52, 9, 132);
+  float: right;
+  cursor: pointer;
 }
 </style>
