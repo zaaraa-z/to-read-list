23cf8a1b1d71454b5d3ca89b981eabd82d2eb147
zaaraa-z <zamanian.zahra@gmail.com>
2022-07-16 19:39:35 +0200

    update styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <a hidden href="https://www.freepik.com/vectors/education-frame"
+      >Education frame vector created by stockgiu - www.freepik.com</a
+    >
+    <h2>So, what is next book?</h2>
     <BooksList></BooksList>
   </div>
 </template>
 
 <script>
 import BooksList from "./components/BooksList.vue";
-
 export default {
   name: "App",
   components: {
@@ -20,12 +23,26 @@ export default {
   box-sizing: border-box;
 }
 
+body {
+  background: url(./assets/bg.jpg) center center no-repeat;
+  background-size: cover;
+  height: 100vh;
+  margin: 0;
+}
+
+h2 {
+  background-color: rgba(25, 5, 77, 0.95);
+  color: rgba(255, 255, 255, 0.88);
+  padding: 20px;
+  margin-bottom: 30px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #1d0934;
+  margin-top: 30px;
 }
 </style>
--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
     <form action="">
-      <input type="text" placeholder="Add the book you wanna read..." />
+      <input type="text" placeholder="Book title..." />
+      <input type="text" placeholder="Book author..." />
+      <input type="text" placeholder="Book pages..." />
+      <button>Save to my list</button>
     </form>
 
     <ul>
@@ -40,7 +43,7 @@ export default {
 <style scoped>
 section {
   width: 80%;
-  max-width: 700px;
+  max-width: 600px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -50,11 +53,40 @@ section {
 
 form {
   width: 100%;
+  border-radius: 5px;
+  border: 1px solid rgb(111, 111, 111);
+  padding: 20px;
+  background-color: rgba(25, 5, 77, 0.732);
 }
 
 form input {
   width: 100%;
   padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0;
+  outline: 0;
+  background-color: rgb(218, 218, 218);
+  border: 1px solid transparent;
+  margin-bottom: 7px;
+}
+form input:focus {
+  border: 1px solid rgb(111, 111, 111);
+  background-color: white;
+}
+
+form button {
+  width: 100%;
+  margin-top: 10px;
+  border: 0;
+  padding: 10px 0;
+  cursor: pointer;
+  background-color: rgb(31, 191, 132);
+  font-size: 15px;
+  transition: 0.3s;
+}
+form button:hover {
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.663);
+  transition: 0.3s;
 }
 
 ul {
@@ -63,14 +95,25 @@ ul {
   text-align: left;
   padding: 0;
 }
+
 li {
-  background-color: rgba(179, 179, 241, 0.529);
+  background-color: rgb(195, 195, 226);
   padding: 6px 20px;
   margin-bottom: 10px;
+  border-radius: 5px;
+  transition: 0.3s;
+}
+li:hover {
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.663);
+  transition: 0.3s;
+}
+li:hover span {
+  color: black;
+  font-weight: bold;
 }
 
 span {
-  color: rgb(52, 9, 132);
+  color: rgb(92, 31, 205);
   float: right;
   cursor: pointer;
 }
